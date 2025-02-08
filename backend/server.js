2e515105bba69092db4e2ffe491e3e3bdeee5e10
Silunisilva <silunisilva2@gmail.com>
2025-02-08 23:08:29 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,31 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.post("/products", async (req, res) =>{
+const __dirname = path.resolve();
 
+app.use(express.json()); // allows us to accept JSON data in the req.body
+
+app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+// Start the server and connect to the database
+app.listen(PORT, () => {
+    connectDB();  // Connect to MongoDB
+    console.log("Server started at http://localhost:" + PORT );
 });
-
-console.log(process.env.MONGO_URI);
-
-app.listen(5001, () => {
-    console.log("server started at http://localhost:5001 hello");
-});
-
-
-//npm install mongodb@5.5
